62f0b465a32d1c5f62e67bd64f9f744da7d8257c
knguyen4221 <kenqn@uci.edu>
2018-01-02 16:10:08 -0800

    Re-implemented in an MVVM model so it's easier to add features

---
+++ ruvseq/ApplicationViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft;
+
+namespace ruvseq
+{
+    public class ApplicationViewModel : GalaSoft.MvvmLight.ViewModelBase
+    {
+        private ICommand _changePageCommand;
+
+        private GalaSoft.MvvmLight.ViewModelBase _currentPageViewModel;
+        private List<GalaSoft.MvvmLight.ViewModelBase> _pageViewModels;
+
+        public ApplicationViewModel()
+        {
+            PageViewModels.Add(new RUVSeqViewModel());
+
+            CurrentPageViewModel = PageViewModels[0];
+        }
+
+        public GalaSoft.MvvmLight.ViewModelBase CurrentPageViewModel
+        {
+            get
+            {
+                return _currentPageViewModel;
+            }
+            set
+            {
+                if(_currentPageViewModel != value)
+                {
+                    _currentPageViewModel = value;
+                    this.RaisePropertyChanged("CurrentPageViewModel");
+                }
+            }
+        }
+
+        public List<GalaSoft.MvvmLight.ViewModelBase> PageViewModels
+        {
+            get
+            {
+                if (_pageViewModels == null)
+                    _pageViewModels = new List<GalaSoft.MvvmLight.ViewModelBase>();
+                return _pageViewModels;
+            }
+        }
+
+        public ICommand ChangePageCommand
+        {
+            get
+            {
+                if(_changePageCommand == null)
+                {
+                    _changePageCommand = new GalaSoft.MvvmLight.Command.RelayCommand<GalaSoft.MvvmLight.ViewModelBase>((p) => ChangeViewModel(p));
+                }
+                return _changePageCommand;
+            }
+        }
+
+        private void ChangeViewModel(GalaSoft.MvvmLight.ViewModelBase viewModel)
+        {
+            _currentPageViewModel = viewModel;
+        }
+    }
+
+}
---
+++ ruvseq/RUVSeq/RUVSeqModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using GalaSoft;
+using RProcesses;
+
+namespace ruvseq
+{
+    public class RUVSeqModel : GalaSoft.MvvmLight.ViewModelBase
+    {
+        private List<string> g1, g2;
+        private string inputFilePath, outputFilePrefix, currentDirectory;
+
+        public List<string> firstGroup
+        {
+            get { return g1; }
+            set
+            {
+                if (value != g1)
+                {
+                    g1 = value;
+                    this.RaisePropertyChanged("firstGroup");
+                }
+            }
+        }
+
+        public List<string> secondGroup
+        {
+            get { return g2; }
+            set
+            {
+                if (value != g2)
+                {
+                    g2 = value;
+                    this.RaisePropertyChanged("secondGroup");
+                }
+            }
+        }
+
+        public string inputFile
+        {
+            get { return this.inputFilePath; }
+            set
+            {
+                if (value != this.inputFilePath)
+                {
+                    this.inputFilePath = value;
+                    this.RaisePropertyChanged("inputFile");
+                }
+            }
+        }
+
+        public string outputPrefix
+        {
+            get { return this.outputFilePrefix; }
+            set
+            {
+                if (value != this.outputFilePrefix)
+                {
+                    this.outputFilePrefix = value;
+                    this.RaisePropertyChanged("outputPrefix");
+                }
+            }
+        }
+
+        public string currDirectory
+        {
+            get { return this.currentDirectory; }
+            set
+            {
+                if (value != this.currentDirectory)
+                {
+                    this.currentDirectory = value;
+                    this.RaisePropertyChanged("currDirectory");
+                }
+            }
+        }
+    }
+}
