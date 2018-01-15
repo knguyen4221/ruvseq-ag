1be52b9b887eb8195bfdfe1f8a4fd8f7fc976599
knguyen4221 <kenqn@uci.edu>
2018-01-15 14:33:48 -0800

    Remove extraneous files

--- ruvseq/src/volplot.R
+++
@@ -1,7 +0,0 @@
-library(manhattanly)
-d <- read.table("S:\\Development\\fastq\\analyses\\graphs\\AST\\counts_differentialExpressionAnalysisWithDESeq2_spikeInNorm.csv",
-                header=TRUE, sep = ",", na.rm)
-colnames(d) <- c(	"X", "baseMean",	"EFFECTSIZE",	"lfcSE",	"stat",	"pvalue", "P")
-volcanoly(d)
-
-
